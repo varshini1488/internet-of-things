83225df80e99c450a0acc8dfacf4d1f088cb5e5c
varshiniacharya <153392139+varshini1488@users.noreply.github.com>
2025-09-09 12:50:55 +0530

    Add files via upload

---
+++ lab6.ino
@@ -0,0 +1,14 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(11,OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(11, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(11, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
